c1bc768bfc8ebbab118297b8368808bcd295a3ac
Rbilli51614 <cybertiger51614@gmail.com>
2025-10-19 16:43:54 -0500

    added google network files

---
+++ modules/gcp/network/main.tf
@@ -0,0 +1,24 @@
+variable "project" { type = string }
+variable "region"  { type = string }
+variable "vpc_name" { 
+  type = string 
+  default = "gcp-vpc" 
+  }
+variable "subnet_cidr" { 
+  type = string 
+  default = "10.20.1.0/24" 
+  }
+
+resource "google_compute_network" "vpc" {
+  name                    = var.vpc_name
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet" {
+  name          = "${var.vpc_name}-subnet"
+  ip_cidr_range = var.subnet_cidr
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+
